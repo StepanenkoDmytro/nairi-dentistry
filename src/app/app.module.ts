0b241b9b49a76a38f1f91a54c3e060c060ed634c
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-12-13 18:33:32 +0200

    add about-area & appointment & hero-area components and refactor style.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroAreaComponent } from './components/hero-area/hero-area.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { AboutAreaComponent } from './components/about-area/about-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroAreaComponent,
+    AppointmentComponent,
+    AboutAreaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/hero-area/hero-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-area',
+  templateUrl: './hero-area.component.html',
+  styleUrls: ['./hero-area.component.scss']
+})
+export class HeroAreaComponent {
+
+}
