dfb6ac8fe5e3546efbe4a84d6ced1df852484f3c
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-02-22 18:53:29 +0200

    new init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { HeroAreaComponent } from './components/hero-area/hero-area.component';
-import { AppointmentComponent } from './components/appointment/appointment.component';
-import { AboutAreaComponent } from './components/about-area/about-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    HeroAreaComponent,
-    AppointmentComponent,
-    AboutAreaComponent
   ],
   imports: [
     BrowserModule,
