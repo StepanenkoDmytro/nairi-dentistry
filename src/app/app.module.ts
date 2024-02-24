b2eb42cb39ebce87b8a1e6383d6f42af3d0812e4
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2024-02-24 18:54:42 +0200

    add services-list components and auxiliary title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PreviewComponent } from './components/preview/preview.component';
+import { ServicesListComponent } from './components/services-list/services-list.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PreviewComponent,
+    ServicesListComponent,
+    TitleComponent,
   ],
   imports: [
     BrowserModule,
